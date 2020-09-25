a1ade2bb8cff3fe4a96ba4c43658da63aa765d36
elsen <skadiri94@yahoo.com>
2020-09-25 10:55:59 +0100

    09/25/2020

---
+++ src/Lab2/CP3LinkedList.java
@@ -0,0 +1,46 @@
+package Lab2;
+
+import java.util.NoSuchElementException;
+
+public class CP3LinkedList <E> {
+    private class Node {
+        public E data;
+        public Node next;
+    }
+
+    private Node first;
+
+    public CP3LinkedList() {
+        first = null;
+    }
+
+    public E getFirst() {
+
+        if (first == null)
+            throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public void addFirst(E element) {
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = first;
+        first = newNode;
+
+    }
+
+    public void removeFirst() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        first = first.next;
+    }
+
+    public boolean isEmpty() {
+/*        if(first == null) return true;
+           else return false;
+    }*/
+        return first == null;
+    }
+
+}
